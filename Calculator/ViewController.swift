715057182b74cf35a1dc42e34eb891aaea7d11bd
Dylan Brown <djwbrown@gmail.com>
2015-12-18 14:34:55 +0200

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dylan Walker Brown on 12/11/15.
+//  Copyright © 2015 Dylan Walker Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var brain = CalculatorBrain()
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0.0
+            }
+        }
+    }
+    
+    var operandStack: Array<Double> = []
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        if let result = brain.pushOperand(displayValue){
+            displayValue = result
+        } else {
+            displayValue = 0.0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+}
